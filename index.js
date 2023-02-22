95c776e7fe20459ecb6354109f675e49efaab279
Nouhi <67205946+NouhiDev@users.noreply.github.com>
2023-02-22 20:59:24 +0100

    D

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+window.alert("I highly recommend enabling hardware acceleration and viewing the simulation in fullscreen.");
+
 $(window).on("load", async function () {
-    // await delay(2000);
+  // await delay(2000);
     $(".loader").fadeOut(1000);
     delay(1000).then(() => $(".content").fadeIn(1000));
   });
@@ -7,4 +9,5 @@ $(window).on("load", async function () {
   function delay(time) {
     return new Promise((resolve) => setTimeout(resolve, time));
   }
+  
   
